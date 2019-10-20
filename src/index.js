a80e1cae9f6b0c12890092f463d35dce2f765373
Tokar Irina <irina.tokar@hexagon.com>
2019-10-20 11:55:41 +0300

    2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 
 const init = () => {
   const data = {
-    gameTime: 15,
-    errorCount: 5,
+    gameTime: 25,
+    errorCount: 15,
   };
   ReactDOM.render(<App gameTime={data.gameTime} errorCount={data.errorCount}/>, document.getElementById(`root`));
 };
