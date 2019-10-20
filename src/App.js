523eaec45c656f5fb897dea482773767f939afca
Tokar Irina <irina.tokar@hexagon.com>
2019-10-20 11:41:37 +0300

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import OpenScreen from './components/OpenScreen';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+  const {gameTime, errorCount} = props;
+
+  return (
+    <OpenScreen
+      time = {gameTime}
+      errorCount = {errorCount}
+    />
+  );
+};
+
+App.defaultProps = {
+  gameTime: 0,
+  errorCount: 0,
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number,
+  errorCount: PropTypes.number,
+};
+
+export default App;
